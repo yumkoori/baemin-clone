1ad4ea3ca72299e23e59b289dff4780fbe5f42ba
keitharing <awooh3398@gmail.com>
2025-08-04 14:59:34 +0900

    review entity commit

--- baemin/src/main/java/com/sist/baemin/store/domain/ReviewEntity.java
+++ baemin/src/main/java/com/sist/baemin/store/domain/ReviewEntity.java
@@ -1,4 +1,57 @@
 package com.sist.baemin.store.domain;
 
+import java.time.LocalDateTime;
+
+import com.sist.baemin.order.domain.OrderEntity;
+import com.sist.baemin.user.domain.UserEntity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "리뷰")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class ReviewEntity {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long reviewId;
+    
+    @ManyToOne
+    @JoinColumn(name = "userId")
+    private UserEntity user;
+    
+    @ManyToOne
+    @JoinColumn(name = "storeId")
+    private StoreEntity store;
+    
+    @OneToOne
+    @JoinColumn(name = "orderId")
+    private OrderEntity order;
+    
+    @Column(name = "rating")
+    private int rating; // 1~5점
+    
+    @Column(name = "content")
+    private String content;
+    
+    @Column(name = "createdAt")
+    private LocalDateTime createdAt;
+    
 }
