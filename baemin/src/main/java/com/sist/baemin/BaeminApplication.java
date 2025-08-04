4e2aa2afb19f8c37754e24115b8a54de6c5c86c3
yumi <yumkoori@gmail.com>
2025-08-04 15:37:15 +0900

    feat: JPA User 엔티티 설계 및 초기 생성

--- baemin/src/main/java/com/sist/baemin/BaeminApplication.java
+++ baemin/src/main/java/com/sist/baemin/BaeminApplication.java
@@ -1,9 +1,14 @@
 package com.sist.baemin;
 
+import jakarta.persistence.EntityListeners;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EntityListeners(AuditingEntityListener.class)
+@EnableJpaAuditing
 public class BaeminApplication {
 
 	public static void main(String[] args) {
--- baemin/src/main/java/com/sist/baemin/user/domain/SocialUserEntity.java
+++ baemin/src/main/java/com/sist/baemin/user/domain/SocialUserEntity.java
@@ -1,4 +1,22 @@
 package com.sist.baemin.user.domain;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "socialUser")
 public class SocialUserEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "socialUserId")
+    private Long socialUserId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "userId", nullable = false)
+    private UserEntity user;
+
+    @Column(name = "provider")
+    private String provider;        //enum으로 변경 가능성
+
+    @Column(name = "providerId")
+    private String providerId;
 }
